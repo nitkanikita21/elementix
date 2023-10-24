587de0809b21b9904f50b4a624394e7170c048c3
NitkaNikita <waleryvvs1@gmail.com>
2023-10-24 19:35:41 +0300

    remove old components

--- elementix_dom/src/jsMain/kotlin/Main.kt
+++ elementix_dom/src/jsMain/kotlin/Main.kt
@@ -1,19 +1,5 @@
-
-import elementix.dom.ReactiveProp
-import elementix.dom.StaticProp
-import elementix.dom.asProp
-import elementix.dom.elementBuilder
-import elementix.dom.tags.Button
-import elementix.dom.tags.Div
-import elementix.dom.tags.P
-import elementix.dom.tags.fc.For
 import elementix.reactivity.Context
 import elementix.reactivity.primitives.ReadSignal
-import elementix.reactivity.primitives.toStringSignal
-import kotlinx.browser.document
-import kotlinx.browser.window
-import kotlin.random.Random
-import kotlin.random.nextUInt
 
 //val cx = Context()
 
@@ -24,49 +10,11 @@ fun main() {
     val list = ReadSignal {
         mutableListOf<Int>().apply {
             console.log("each")
-            for (i in 0..count.get()){
+            for (i in 0..count.get()) {
                 add(count.get())
             }
         }
     }
 
-
-    document.getElementById("root")!!.elementBuilder {
-        +Button(cx) {
-            props {
-                onClick = StaticProp {
-                    count(count()-1)
-                }
-                innerText = StaticProp("-")
-            }
-        }
-        +P(cx) {
-            props {
-                innerText = ReactiveProp(count.toStringSignal())
-            }
-        }
-        +Button(cx) {
-            props {
-                onClick = StaticProp {
-                    count(count()+1)
-                }
-                innerText = StaticProp("+")
-            }
-        }
-
-        +For<Int>(cx) {
-            each = ReactiveProp(list)
-            generator = { it: Int ->
-                P(cx) {
-                    props { innerText = it.toString().asProp() }
-                }
-            }.asProp()
-        }
-        +P(cx) {
-            props {
-                innerText = "###################################".asProp()
-            }
-        }
-    }
 
 }
